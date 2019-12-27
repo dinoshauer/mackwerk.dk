1e992a24fcdc1f2b7239b2896ffcff391e346bbc
Kasper Jacobsen <k@mackwerk.dk>
2019-12-27 13:01:54 +0100

    Add js and experiences

---
+++ js/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const waveEl = document.querySelector('.wave');
+  setPopIn(waveEl);
+});
+
+function setPopIn (el) {
+  setTimeout(() => {
+    el.style.animationName = 'pop-in';
+    el.style.animationDuration = '.5s';
+  }, 500)
+
+  el.addEventListener('animationend', () => {
+    el.style.transform = 'scale(1)';
+    el.style.animationName = 'wave';
+    el.style.animationDuration = '1s';
+    el.style.animationDelay = '.5s';
+    el.style.animationIterationCount = 'infinite';
+  })
+}
